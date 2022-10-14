e70a790db27e9e3a3de8aa65c22ade9ce6f98536
Bushra209 <115788536+Bushra209@users.noreply.github.com>
2022-10-14 13:41:18 +0300

    Add files via upload

---
+++ Assigned3.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class Assigned3 {
+    public static void main(String[] args){
+
+        Assigned3 c=new
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import java.util.Scanner;
+
+public class Assigned3 {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.println("inter three numbers");
+        int x =input.nextInt();
+        int y =input.nextInt();
+        int z =input.nextInt();
+        if (x==y&&x==z){
+            System.out.println("all the same");
+        }
+        else {
+            System.out.println("all different");
+        }
+    }
+}
+*/
